0e7ebe73286fce1619b83256c39120237ce190d4
Jorge Frias <jafrias@ibm.com>
2020-04-19 21:20:25 -0500

    csvToJson app

---
+++ main.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+const csv = require("csvtojson");
+const folderName = "jsonFolder";
+const fileName = "customer-data.json";
+const jsonFilePath = path.join(__dirname, folderName, fileName);
+const csvFilePath = path.join(__dirname, "customer-data.csv");
+const convertCsvToJson = async () => {
+  const jsonArray = await csv().fromFile(csvFilePath);
+  fs.existsSync(folderName) ? true : fs.mkdirSync(folderName);
+  fs.writeFileSync(jsonFilePath, JSON.stringify(jsonArray));
+};
+convertCsvToJson();

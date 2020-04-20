b0624b888ab68946551187101251327610e8d386
Jorge Frias <jafrias@ibm.com>
2020-04-19 21:46:39 -0500

    json format

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ const csvFilePath = path.join(__dirname, "customer-data.csv");
 const convertCsvToJson = async () => {
   const jsonArray = await csv().fromFile(csvFilePath);
   fs.existsSync(folderName) ? true : fs.mkdirSync(folderName);
-  fs.writeFileSync(jsonFilePath, JSON.stringify(jsonArray));
+  fs.writeFileSync(jsonFilePath, JSON.stringify(jsonArray, null, 2));
 };
 convertCsvToJson();
